edab33b9586093c9c5cd19c3acb9b00affd09fd8
George Asiedu <georgeasiedu543@gmail.com>
2024-06-02 23:31:40 +0000

    remove default styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>25 + 5 Clock</h1>
+      <div id="clock">
+        <div id="break-label">Break Length</div>
+        <div id="break-length">5</div>
+        <button id="break-decrement">-</button>
+        <button id="break-increment">+</button>
+        <div id="session-label">Session Length</div>
+        <div id="session-length">25</div>
+        <button id="session-decrement">-</button>
+        <button id="session-increment">+</button>
+        <div id="timer-label">Session</div>
+        <div id="time-left">25:00</div>
+        <button id="start_stop">Start/Stop</button>
+        <button id="reset">Reset</button>
+        <audio
+          id="beep"
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        ></audio>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
